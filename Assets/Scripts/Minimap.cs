009c705f94d5a4aeffad06ae0d0977720f774ee2
Hexaminer <16254422+hexaminer@users.noreply.github.com>
2018-10-23 11:23:46 +0100

    ADD Minimap

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+
+    public Transform playerTrabsform;
+
+    /*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    */
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = playerTrabsform.position;
+        newPosition.y = 90f;//playerTrabsform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, playerTrabsform.eulerAngles.y, 0f);
+
+    }
+}
